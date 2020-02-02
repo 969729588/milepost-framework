820caad7aa86173d6589c8b200b86387779d0468
Huarf <969729588@qq.com>
2020-02-02 11:57:57 +0800

    完成认证，swagger-ui

--- milepost-api/src/test/java/com/milepost/api/util/MyTest.java
+++ milepost-api/src/test/java/com/milepost/api/util/MyTest.java
@@ -9,6 +9,9 @@ import java.io.File;
  * Created by Ruifu Hua on 2020/1/16.
  */
 public class MyTest {
+    /**
+     * test
+     */
     @Test
     public void test1(){
         FileUtils.deleteQuietly(new File("F:\\testFile\\testZip_"));
