2eb9ca9c82abbcb0215cee67b06b4201a27d7b17
Huarf <969729588@qq.com>
2020-03-20 21:21:03 +0800

    解决token过期后重新登录，不能识别FeignClient方法入参中的token的问题。

--- milepost-service/src/main/java/com/milepost/service/config/openfeign/FeignClientInterceptor.java
+++ milepost-service/src/main/java/com/milepost/service/config/openfeign/FeignClientInterceptor.java
@@ -10,6 +10,9 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Ruifu Hua on 2020/1/20.
@@ -18,23 +21,51 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 public class FeignClientInterceptor implements RequestInterceptor {
 
+    private final static String AUTHORIZATION = "Authorization";
+
     /**
-     * token放在请求头.
+     * token放在请求头.优先使用请求头中的token
      *
      * @param requestTemplate 请求参数
      */
     @Override
     public void apply(RequestTemplate requestTemplate) {
+        //如果requestTemplate中有token，则不从HttpServletRequest中拿token，
+
+        //requestTemplate中的token：FeignClient方法入参中的@RequestHeader(value = "Authorization") String authorization
+        //HttpServletRequest中的token：前端请求传入的token，可以在请求头中，也可以在参数中
+
+        //解决认证UI登录接口(com.milepost.authenticationUi.auth.controller.LoginController#login)中,
+        //旧token过期后仍存在于前端缓存中，重新登录时获取到新token之后请求认证Service时，次此处无法识别FeignClient方法入参中的token的问题。
+        if(requestTemplateHasToken(requestTemplate)){
+            return;
+        }
+
+        //requestTemplate中没有token时，从HttpServletRequest中获取
         RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
-        if (requestAttributes != null) {
-            HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-            String tokenHeader = request.getHeader("Authorization");
-            String tokenParameter = request.getParameter("access_token");
-            if(StringUtils.isNotBlank(tokenHeader)){
-                requestTemplate.header("Authorization", new String[]{tokenHeader});
-            }else if(StringUtils.isNotBlank(tokenParameter)){
-                requestTemplate.header("Authorization", new String[]{"Bearer " + tokenParameter});
-            }
+
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        String tokenHeader = request.getHeader(AUTHORIZATION);
+        String tokenParameter = request.getParameter("access_token");
+        if(StringUtils.isNotBlank(tokenHeader)){
+            requestTemplate.header(AUTHORIZATION, new String[]{tokenHeader});
+        }else if(StringUtils.isNotBlank(tokenParameter)){
+            requestTemplate.header(AUTHORIZATION, new String[]{"Bearer " + tokenParameter});
+        }
+    }
+
+    /**
+     * 如果requestTemplate中有token，则返回true，否则返回false
+     * @param requestTemplate
+     * @return
+     */
+    private boolean requestTemplateHasToken(RequestTemplate requestTemplate) {
+        Map<String, Collection<String>> headers = requestTemplate.headers();
+        Set<String> headerNames = headers.keySet();
+        if(headerNames.contains(AUTHORIZATION)){
+            return true;
+        }else{
+            return false;
         }
     }
 }
--- milepost-ui/src/main/java/com/milepost/ui/config/openfeign/FeignClientInterceptor.java
+++ milepost-ui/src/main/java/com/milepost/ui/config/openfeign/FeignClientInterceptor.java
@@ -10,6 +10,9 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Ruifu Hua on 2020/1/20.
@@ -18,6 +21,8 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 public class FeignClientInterceptor implements RequestInterceptor {
 
+    private final static String AUTHORIZATION = "Authorization";
+
     /**
      * token放在请求头.优先使用请求头中的token
      *
@@ -25,16 +30,42 @@ public class FeignClientInterceptor implements RequestInterceptor {
      */
     @Override
     public void apply(RequestTemplate requestTemplate) {
+        //如果requestTemplate中有token，则不从HttpServletRequest中拿token，
+
+        //requestTemplate中的token：FeignClient方法入参中的@RequestHeader(value = "Authorization") String authorization
+        //HttpServletRequest中的token：前端请求传入的token，可以在请求头中，也可以在参数中
+
+        //解决认证UI登录接口(com.milepost.authenticationUi.auth.controller.LoginController#login)中,
+        //旧token过期后仍存在于前端缓存中，重新登录时获取到新token之后请求认证Service时，次此处无法识别FeignClient方法入参中的token的问题。
+        if(requestTemplateHasToken(requestTemplate)){
+           return;
+        }
+
+        //requestTemplate中没有token时，从HttpServletRequest中获取
         RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
-        if (requestAttributes != null) {
-            HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-            String tokenHeader = request.getHeader("Authorization");
-            String tokenParameter = request.getParameter("access_token");
-            if(StringUtils.isNotBlank(tokenHeader)){
-                requestTemplate.header("Authorization", new String[]{tokenHeader});
-            }else if(StringUtils.isNotBlank(tokenParameter)){
-                requestTemplate.header("Authorization", new String[]{"Bearer " + tokenParameter});
-            }
+
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        String tokenHeader = request.getHeader(AUTHORIZATION);
+        String tokenParameter = request.getParameter("access_token");
+        if(StringUtils.isNotBlank(tokenHeader)){
+            requestTemplate.header(AUTHORIZATION, new String[]{tokenHeader});
+        }else if(StringUtils.isNotBlank(tokenParameter)){
+            requestTemplate.header(AUTHORIZATION, new String[]{"Bearer " + tokenParameter});
+        }
+    }
+
+    /**
+     * 如果requestTemplate中有token，则返回true，否则返回false
+     * @param requestTemplate
+     * @return
+     */
+    private boolean requestTemplateHasToken(RequestTemplate requestTemplate) {
+        Map<String, Collection<String>> headers = requestTemplate.headers();
+        Set<String> headerNames = headers.keySet();
+        if(headerNames.contains(AUTHORIZATION)){
+            return true;
+        }else{
+            return false;
         }
     }
 }
