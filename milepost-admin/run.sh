def2b136992f0585d52e60c6d4e47bcc41389f91
Huarf <969729588@qq.com>
2020-03-30 19:52:12 +0800

    修复admin发送邮件bug

--- milepost-admin/run.sh
+++ milepost-admin/run.sh
@@ -8,13 +8,13 @@ start() {
     --spring.profiles.active=test \
     --server.port=8768 \
     --eureka.instance.ip-address=192.168.223.129 \
-    --multiple-tenant.tenant=tenant1 \
     --spring.boot.admin.ui.title=milepost-admin \
     --spring.mail.username=m18310891237@163.com \
     --spring.mail.password=sqm123456qweasd \
     --spring.mail.host=smtp.163.com \
     --spring.boot.admin.notify.mail.to=969729588@qq.com \
     --spring.boot.admin.discovery.ignored-services=test-service \
+    --multiple-tenant.tenant=tenant1 \
     >/dev/null 2>&1 &
 }
 
