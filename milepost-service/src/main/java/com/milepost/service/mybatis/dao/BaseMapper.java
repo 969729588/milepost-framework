0f61b099a0524f0b86f06588337b010fad8c1b11
花瑞富 <huarf@neunn.com>
2020-05-20 21:58:49 +0800

    修改注释性错误

--- milepost-service/src/main/java/com/milepost/service/mybatis/dao/BaseMapper.java
+++ milepost-service/src/main/java/com/milepost/service/mybatis/dao/BaseMapper.java
@@ -19,49 +19,49 @@ public interface BaseMapper<M, E> {
 	 * @param example 传null表示查询所有
 	 * @return
 	 */
-	public List<M> selectByExample(E example);
+	List<M> selectByExample(E example);
 
 	/**
 	 * 根据id查询数据，
 	 * @param id
 	 * @return
 	 */
-	public M selectByPrimaryKey(String id);
+	M selectByPrimaryKey(String id);
 
 	/**
 	 * 根据条件删除数据，
 	 * @param example 传null表示删除所有
 	 * @return
 	 */
-	public int deleteByExample(E example);
+	int deleteByExample(E example);
 
 	/**
 	 * 根据id删除数据，
 	 * @param id
 	 * @return
 	 */
-	public int deleteByPrimaryKey(String id);
+	int deleteByPrimaryKey(String id);
 
 	/**
 	 * 针对所有字段插入数据，为null的字段将被插入null
 	 * @param record
 	 * @return
 	 */
-	public int insert(M record);
+	int insert(M record);
 
 	/**
 	 * 针对非null字段插入数据，为null的字段将使用数据库默认值
 	 * @param record
 	 * @return
 	 */
-	public int insertSelective(M record);
+	int insertSelective(M record);
 
 	/**
 	 * 根据条件查询数据个数，
 	 * @param example 传null表示查询所有
 	 * @return
 	 */
-	public int countByExample(E example);
+	int countByExample(E example);
 
 	/**
 	 * 根据条件，针对非null字段更新数据，为null的字段将保留原来的值
@@ -69,7 +69,7 @@ public interface BaseMapper<M, E> {
 	 * @param example 传null表示更新所有
 	 * @return
 	 */
-	public int updateByExampleSelective(@Param("record") M record, @Param("example") E example);
+	int updateByExampleSelective(@Param("record") M record, @Param("example") E example);
 
 	/**
 	 * 根据条件，针对所有字段更新数据，为null的字段将被赋值成null
@@ -77,20 +77,20 @@ public interface BaseMapper<M, E> {
 	 * @param example 传null表示更新所有
 	 * @return
 	 */
-	public int updateByExample(@Param("record") M record, @Param("example") E example);
+	int updateByExample(@Param("record") M record, @Param("example") E example);
 
 	/**
 	 * 根据id，针对非null字段更新数据，为null的字段将保留原来的值
 	 * @param record
 	 * @return
 	 */
-	public int updateByPrimaryKeySelective(M record);
+	int updateByPrimaryKeySelective(M record);
 
 	/**
 	 * 根据id，针对所有字段更新数据，为null的字段将被赋值成null
 	 * @param record
 	 * @return
 	 */
-	public int updateByPrimaryKey(M record);
+	int updateByPrimaryKey(M record);
     
 }
